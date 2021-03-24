9d68ff65a94ad6cf1b6b30fd26de8d0156581837
V1PEX7 <micbel2000@gmail.com>
2021-03-24 13:56:49 +0300

    V1.1

--- DWM/DefaultThemes.cs
+++ DWM/DefaultThemes.cs
@@ -8,10 +8,10 @@ namespace DWM
 {
     class DefaultThemes
     {
-        public Dictionary<string, string> themes = new Dictionary<string,string>();
+        public Dictionary<string, string> themes = new Dictionary<string, string>();
         public DefaultThemes()
         {
-            themes.Add("default","[Gradient 1 Color 1]\r\n#FF516395\r\n[Gradient 1 Color 2]\r\n#FF2B5876\r\n[Gradient 2 Color 1]\r\n#FF2B5876\r\n[Gradient 2 Color 2]\r\n#FF4E4376\r\n[Background Gradient Color 1]\r\n#FF04619F\r\n[Background Gradient Color 2]\r\n#000000\r\n[Sidebar Gradient Color 1]\r\n#FF232220\r\n[Sidebar Gradient Color 2]\r\n#FF232220\r\n");
+            themes.Add("default", "[Gradient 1 Color 1]\r\n#FF516395\r\n[Gradient 1 Color 2]\r\n#FF2B5876\r\n[Gradient 2 Color 1]\r\n#FF2B5876\r\n[Gradient 2 Color 2]\r\n#FF4E4376\r\n[Background Gradient Color 1]\r\n#FF04619F\r\n[Background Gradient Color 2]\r\n#000000\r\n[Sidebar Gradient Color 1]\r\n#FF232220\r\n[Sidebar Gradient Color 2]\r\n#FF232220\r\n");
             themes.Add("sunset", "[Gradient 1 Color 1]\r\n#000048\r\n[Gradient 1 Color 2]\r\n#830048\r\n[Gradient 2 Color 1]\r\n#000048\r\n[Gradient 2 Color 2]\r\n#830048\r\n[Background Gradient Color 1]\r\n#FF232220\r\n[Background Gradient Color 2]\r\n#00002A\r\n[Sidebar Gradient Color 1]\r\n#FF232220\r\n[Sidebar Gradient Color 2]\r\n#FF232220\r\n");
             themes.Add("solid", "[Gradient 1 Color 1]\r\n#38383d\r\n[Gradient 1 Color 2]\r\n#38383d\r\n[Gradient 2 Color 1]\r\n#38383d\r\n[Gradient 2 Color 2]\r\n#38383d\r\n[Background Gradient Color 1]\r\n#2a2a2e\r\n[Background Gradient Color 2]\r\n#2a2a2e\r\n[Sidebar Gradient Color 1]\r\n#2a2a2e\r\n[Sidebar Gradient Color 2]\r\n#2a2a2e\r\n");
         }
--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # DiscordWebhookManager
-A program for storing and using discord webhooks. Supports changing themes and storing avatars for use by webhooks.
-To add themes you can drag them into a themes folder (there is a button in the settings to open it)
-You can create your own themes: create a copy of the theme, rename it, open it as a text file and change the data.
+A program for storing and using discord webhooks. Supports changing themes and storing avatars for use by webhooks.<br/>
+To add themes you can drag them into a themes folder (there is a button in the settings to open it)<br/>
+You can create your own themes: create a copy of the theme, rename it, open it as a text file and change the data.<br/> <br/>
+Send menu:
 
 ![Send](https://i.ibb.co/L1DYQMX/image.png)
+
+Add menu:
+
 ![Add](https://i.ibb.co/Mg5KzvW/image.png)
+
+Delete menu:
+
 ![Delete](https://i.ibb.co/rHmHd8M/image.png)
+
+Settings menu:
+
 ![Settings](https://i.ibb.co/GMTYQ4f/image.png)
 
 ## Themes:
